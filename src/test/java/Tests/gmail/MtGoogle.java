36a8b5e5ed2e2a73a1b19d21c564cb1734e1ad97
qainfotech <qainfotech@asda1712po02529.qait.com>
2017-07-21 13:09:21 +0530

    change of versions through the terminal by mvn

--- src/test/java/Tests/gmail/MtGoogle.java
+++ src/test/java/Tests/gmail/MtGoogle.java
@@ -12,10 +12,14 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.*;
+
+import utility.Datadecider;
+
 import com.google.api.services.sheets.v4.Sheets;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -29,7 +33,15 @@ import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
 
+import org.apache.oltu.oauth2.client.OAuthClient;
+import org.apache.oltu.oauth2.client.URLConnectionClient;
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.client.response.OAuthJSONAccessTokenResponse;
+import org.apache.oltu.oauth2.common.message.types.GrantType;
+
 public class MtGoogle {
+
+	// for getting https://developers.google.com/oauthplayground
 
 	/** Application name. */
 	private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
@@ -117,14 +129,16 @@ public class MtGoogle {
 				.build();
 	}
 
-	public static void main6(String[] args) throws Exception {
-		
-		
-		//https://docs.google.com/spreadsheets/d/1n6DeLGHKvUd-_mziCo5Q5Juuq6w4cCpw2mqY46oCZL0/edit?usp=sharing
-		//https://docs.google.com/spreadsheets/d/1HluvvKnFcWTXf-ybcR97te2bTjVRUDWy9JdYU9f8ObU/edit?usp=sharing
-		//https://docs.google.com/spreadsheets/d/1HluvvKnFcWTXf-ybcR97te2bTjVRUDWy9JdYU9f8ObU/edit?usp=sharing
+	public static void mainT(String[] args) throws Exception {
+
+		// https://docs.google.com/spreadsheets/d/1n6DeLGHKvUd-_mziCo5Q5Juuq6w4cCpw2mqY46oCZL0/edit?usp=sharing
+		// https://docs.google.com/spreadsheets/d/1HluvvKnFcWTXf-ybcR97te2bTjVRUDWy9JdYU9f8ObU/edit?usp=sharing
+		// https://docs.google.com/spreadsheets/d/1HluvvKnFcWTXf-ybcR97te2bTjVRUDWy9JdYU9f8ObU/edit?usp=sharing
+		// URL url = new URL(
+		// "https://sheets.googleapis.com/v4/spreadsheets/1n6DeLGHKvUd-_mziCo5Q5Juuq6w4cCpw2mqY46oCZL0/values/A1:C5?key=%20AIzaSyCwt2tn6wfyJY646Y-2DWYlRqVfkXqqkEg");
+
 		URL url = new URL(
-				"https://sheets.googleapis.com/v4/spreadsheets/1n6DeLGHKvUd-_mziCo5Q5Juuq6w4cCpw2mqY46oCZL0/values/A1:C5?key=%20AIzaSyCwt2tn6wfyJY646Y-2DWYlRqVfkXqqkEg");
+				"https://sheets.googleapis.com/v4/spreadsheets/1n6DeLGHKvUd-_mziCo5Q5Juuq6w4cCpw2mqY46oCZL0/values/A:F?access_token=ya29.GluOBC9EiyacIoMQtgAZlFbt-Xv_SGYivFWTfNDI86JxMRHTZVaK3lI57dxUgvNuaOr_zeUKH0DCCtmWLbpUjjgoKs_vvIEwkyYJ-Ufz9eq8sMQpCHBXGpGoV9BH");
 
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		connection.setDoOutput(true);
@@ -142,15 +156,21 @@ public class MtGoogle {
 
 	}
 
-	public static void main(String[] args) throws Exception {
-		
-		URL url = new URL(
-				"https://sheets.googleapis.com/v4/spreadsheets/1n6DeLGHKvUd-_mziCo5Q5Juuq6w4cCpw2mqY46oCZL0/values/A:F?key=%20AIzaSyCwt2tn6wfyJY646Y-2DWYlRqVfkXqqkEg");
-Object data = "67868";
-		String message = "{ \"spreadsheetId \": \"1n6DeLGHKvUd-_mziCo5Q5Juuq6w4cCpw2mqY46oCZL0\",\"tableRange\": \"A1:A1\",\"updates\":  "+ data +" }";
+	public static void maint(String[] args) throws Exception {
 
-		System.out.println(message);
-		
+		// URL url = new URL(
+		// "https://sheets.googleapis.com/v4/spreadsheets/1n6DeLGHKvUd-_mziCo5Q5Juuq6w4cCpw2mqY46oCZL0/values/A:F:clear?access_token=ya29.GluOBC9EiyacIoMQtgAZlFbt-Xv_SGYivFWTfNDI86JxMRHTZVaK3lI57dxUgvNuaOr_zeUKH0DCCtmWLbpUjjgoKs_vvIEwkyYJ-Ufz9eq8sMQpCHBXGpGoV9BH");
+
+		URL url = new URL(
+				"https://sheets.googleapis.com/v4/spreadsheets/1n6DeLGHKvUd-_mziCo5Q5Juuq6w4cCpw2mqY46oCZL0/values/A:B:clear?access_token=ya29.GluOBKprFqJj0fJvEoBUQamvF3JaKq73G8THuEGfkAHKwiXyc7H3NwEwulOJPhYheHFhBvf6YpCzdV0bOSapMxcL2gwhl5awzZK1uFrUzWD2WTCn5kvNDlRuZcw_");
+
+		// Object data = "67868";
+		// String message = "{ \"spreadsheetId \":
+		// \"1n6DeLGHKvUd-_mziCo5Q5Juuq6w4cCpw2mqY46oCZL0\",\"tableRange\":
+		// \"A1:A1\",\"updates\": "+ data +" }";
+		//
+		// System.out.println(message);
+		//
 		try {
 			// instantiate the URL object with the target URL of the resource to
 			// request
@@ -159,22 +179,121 @@ Object data = "67868";
 
 			connection.setDoOutput(true);
 			connection.setRequestMethod("POST");
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setFixedLengthStreamingMode(00);
+			// connection.connect();
 
-			OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+			//
+			// OutputStreamWriter writer = new
+			// OutputStreamWriter(connection.getOutputStream());
+			//
+			// writer.write(message);
+			//
+			// writer.close();
+			//
 
-			writer.write(message);
+			BufferedReader br = new BufferedReader(new InputStreamReader((connection.getInputStream())));
 
-			writer.close();
-		
+			String line = br.readLine();
+			while (line != null) {
+
+				line = br.readLine();
+				System.out.println(line);
+
+			}
+
 			System.out.println(connection.getResponseCode());
-		
 
 		} catch (IOException e) {
 			// ...
-			
+
 			e.printStackTrace();
 		}
 
+		// new MtGoogle().get_data();
+
 	}
 
+	public static void main(String[] args) throws Exception {
+
+		// URL url = new URL(
+		// "https://sheets.googleapis.com/v4/spreadsheets/1n6DeLGHKvUd-_mziCo5Q5Juuq6w4cCpw2mqY46oCZL0/values/A:F:clear?access_token=ya29.GluOBC9EiyacIoMQtgAZlFbt-Xv_SGYivFWTfNDI86JxMRHTZVaK3lI57dxUgvNuaOr_zeUKH0DCCtmWLbpUjjgoKs_vvIEwkyYJ-Ufz9eq8sMQpCHBXGpGoV9BH");
+
+		URL url = new URL(
+				"https://sheets.googleapis.com/v4/spreadsheets/1n6DeLGHKvUd-_mziCo5Q5Juuq6w4cCpw2mqY46oCZL0/values/A1:A1:append?valueInputOption=USER_ENTERED&access_token=ya29.GluOBKprFqJj0fJvEoBUQamvF3JaKq73G8THuEGfkAHKwiXyc7H3NwEwulOJPhYheHFhBvf6YpCzdV0bOSapMxcL2gwhl5awzZK1uFrUzWD2WTCn5kvNDlRuZcw_");
+
+		String message ="";
+//				"{" + " \"range\": \"A1:A1\", " + "  \"majorDimension\": \"ROWS\", " + "  \"values\": [ "
+//				+ "  [\"shadab\",\"nishant\"]" + "]" + "}";
+
+		System.out.println(message);
+
+		try {
+			// instantiate the URL object with the target URL of the resource to
+			// request
+
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+			connection.setDoOutput(true);
+			connection.setRequestMethod("POST");
+			connection.setRequestProperty("Content-Type", "application/json");
+			 connection.setFixedLengthStreamingMode(0);
+			 
+			// connection.connect();
+
+			OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+			// DataOutputStream writer = new
+			// DataOutputStream(connection.getOutputStream());
+
+			writer.write(message);
+		
+
+			writer.close();
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((connection.getInputStream())));
+
+			String line = br.readLine();
+			while (line != null) {
+
+				line = br.readLine();
+				System.out.println(line);
+
+			}
+
+			System.out.println(connection.getResponseCode());
+
+		} catch (IOException e) {
+			// ...
+
+			e.printStackTrace();
+		}
+
+		// new MtGoogle().get_data();
+
+	}
+
+	String get_data() {
+
+		try {
+
+			OAuthClient client = new OAuthClient(new URLConnectionClient());
+			OAuthClientRequest request = OAuthClientRequest.tokenLocation("https://accounts.google.com/o/oauth2/token")
+					.setGrantType(GrantType.CLIENT_CREDENTIALS)
+					.setClientId("901927711216-7oeeh9dm5sgsdli1r5c631i40t48qe4t.apps.googleusercontent.com")
+					.setClientSecret("08uAyVE58BjyvyiJD5vux33t")
+					// .setScope() here if you want to set the token scope
+					.buildQueryMessage();
+
+			String token = client.accessToken(request, OAuthJSONAccessTokenResponse.class).getAccessToken();
+
+			System.out.println(token);
+
+			return token;
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return null;
+
+	}
 }
